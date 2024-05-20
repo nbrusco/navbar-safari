df1a1d321cab9686d48abcb5c9997ce634332937
Nicolas Brusco <nicolasbrusco@hotmail.com>
2024-05-20 17:42:22 -0300

    more chgs vC76

--- react-ts/src/components/Navbar/Navbar.tsx
+++ react-ts/src/components/Navbar/Navbar.tsx
@@ -25,11 +25,11 @@ const NavBar = () => {
 
   return (
     <Navbar className="relative w-full rounded-none px-3 tablet:px-6 tablet:pb-2 pt-9 bg-[#0D0D0D] border-[#0D0D0D] z-10">
-      <div className="flex items-center justify-center mx-auto">
+      <div className="flex items-center justify-center">
         <div className="hidden tablet:block">
           <NavList />
         </div>
-        <IconButton
+        {/* <IconButton
           variant="text"
           className="h-6 w-6 hover:bg-transparent focus:bg-transparent active:bg-transparent tablet:hidden"
           ripple={false}
@@ -40,7 +40,7 @@ const NavBar = () => {
           ) : (
             <Bars3Icon className="h-6 w-6" strokeWidth={2} />
           )}
-        </IconButton>
+        </IconButton> */}
       </div>
       {/* <Collapse open={openNav}>
         <NavList />
