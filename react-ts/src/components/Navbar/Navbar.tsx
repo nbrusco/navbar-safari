19e51aa2285d8259e68b697d07ce80b6b6d4eb78
Nicolas Brusco <nicolasbrusco@hotmail.com>
2024-05-20 17:38:01 -0300

    more chgs v5

--- react-ts/src/components/Navbar/Navbar.tsx
+++ react-ts/src/components/Navbar/Navbar.tsx
@@ -26,13 +26,13 @@ const NavBar = () => {
   return (
     <Navbar className="relative w-full rounded-none px-3 tablet:px-6 tablet:pb-2 pt-9 bg-[#0D0D0D] border-[#0D0D0D] z-10">
       <div className="flex items-center justify-center mx-auto">
-        <Link to={paths.home} className='min-w-[96px] max-w-[250px] w-20 lg:w-36 hd:w-44'>
+        {/* <Link to={paths.home} className='min-w-[96px] max-w-[250px] w-20 lg:w-36 hd:w-44'>
           <img
             src={`/assets/logos/asd.jpg`}
             alt="Logo Zona"
             className="w-full tablet:-mb-8 hd:-mb-12 3xl:-mb-16 min-w-[96px] max-w-[250px]"
           />
-        </Link>
+        </Link> */}
         <div className="hidden tablet:block">
           <NavList />
         </div>
